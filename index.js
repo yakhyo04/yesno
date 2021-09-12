1b12953e6bc3457b389e589d3fb8729017ffbeac
yakhyo04 <fenixrelaxation@gmail.com>
2021-09-12 18:47:05 +0500

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const elForm = document.querySelector('#form');
 const elInput = document.querySelector('#input');
-const elCheckboxFirst = document.querySelector('#checkboxfirst');
+const elCheckbox = document.querySelector('#checkbox');
 const elCheckboxSec = document.querySelector('#checkboxsec');
 const elSpan = document.querySelector('#span');
 const elSpan2 = document.querySelector('#span2');
@@ -13,12 +13,12 @@ elForm.addEventListener('submit', function(e){
         elSpan.classList.add('yes')
     }
 
-    if(elCheckboxFirst.checked &&  elInput.value >= 5 && elInput.value < 30){
+    if(elCheckbox.checked &&  elInput.value >= 5 && elInput.value < 30){
         // elSpan.classList.remove('yes')
         elSpan2.classList.add('no')
     }
 
-    if(elCheckboxFirst.checked && elCheckboxSec.checked && elInput.value >= 5 && elInput.value < 30){
+    if(elCheckbox.checked && elCheckboxSec.checked && elInput.value >= 5 && elInput.value < 30){
         // elSpan2.classList.remove('no')
         elSpan.classList.add('yes')
     }
